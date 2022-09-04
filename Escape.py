62dbb24878b3405bb9a6e3a9f2b46e5222383cec
aleksandr <mkhllv7@gmail.com>
2022-09-04 20:37:29 +0200

    add escape

---
+++ Escape.py
@@ -0,0 +1,7 @@
+print('\\a   Bell (alert)'
+      '\n''\\b   Backspace'
+      '\n''\\n   New line'
+      '\n''\\t   Horizontal tab'
+      '\n' "\\\\" '   Backslash \\'
+      '\n''\\"   Double quotation mark "'
+      '\n''\\',   '   Single quotation mark' '')
