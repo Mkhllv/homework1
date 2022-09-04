46d6bf1ccaa294633cfa844345a30942badbd473
aleksandr <mkhllv7@gmail.com>
2022-09-04 20:15:45 +0200

    all my name

--- myname.py
+++ myname.py
@@ -1,1 +1,11 @@
-print('\t''***' ' \n*\t\t''  *'' \n*\t\t''  *'' \n*\t\t''  *'' \n''\t''***')
+a = '*'
+b = ' '
+c = '\n'
+d = '\t'
+print(b*2+a*7 + b*4+a + b*9+a*7+a + b*2+a+b*8+a + b*2+a*8 + b*8+a + b*8+a+b*7+a + b*2+a*8 + b*4+a*7+d +
+      c+a+b*9 + a+b*2+a + b*9+a + b*9+a+b*6+a + b*4+a + b*14+a+b+a + b*7+a+b*1+a+b*5+a + b*2+a+b*7+a + b*3+a+b*7+a+d +
+      c+a+b*9 + a+b*2+a + b*9+a + b*9+a+b*4+a + b*6+a + b*13+a+b*3+a + b*6+a+b*2+a+b*4+a + b*2+a+b*8+a + b*2+a+b*7+a+d +
+      c+a+b*9 + a+b*2+a + b*9+a*7+a + b*2+a+b*2+a + b*8+a*8 + b*5+a*7 + b*5+a+b*3+a+b*3+a + b*2+a+b*8+a + b*2+a*7+d +
+      c+a+b*9 + a+b*2+a + b*9+a + b*9+a+b*4+a + b*13+a + b*4+a+b*7+a + b*4+a+b*4+a+b*2+a + b*2+a+b*8+a + b*2+a+b*2+a+d +
+      c+a+b*9 + a+b*2+a + b*9+a + b*9+a+b*6+a + b*11+a + b*3+a+b*9+a + b*3+a+b*5+a+b+a + b*2+a+b*7+a + b*3+a+b*4+a+d +
+      c+b*2+a*7 + b*4+a*8 + b*2+a*7+a + b*2+a+b*8+a + b*2+a*8 + b*2+a+b*11+a + b*2+a+b*7+a + b*2+a*8 + b*4+a+b*6+a)
