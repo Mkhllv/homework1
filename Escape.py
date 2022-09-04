0a0e60139b07811f80b576d184a78bbf31175fca
aleksandr <mkhllv7@gmail.com>
2022-09-04 22:32:33 +0200

    change escape

--- Escape.py
+++ Escape.py
@@ -1,7 +1,8 @@
-print('\\a   Bell (alert)'
-      '\n''\\b   Backspace'
-      '\n''\\n   New line'
-      '\n''\\t   Horizontal tab'
-      '\n' "\\\\" '   Backslash \\'
-      '\n''\\"   Double quotation mark "'
-      '\n''\\',   '   Single quotation mark' '')
+print('Escape sequences'
+      '\n \\a   Bell (alert)'
+      '\n \\b   Backspace'
+      '\n \\n   New line'
+      '\n \\t   Horizontal tab'
+      '\n \\\   Backslash \\'
+      '\n \\"   Double quotation mark "'
+      "\n \\'   Single quotation mark'")
